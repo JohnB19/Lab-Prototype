b1f30bef6c3400706c9c94054071d394bb8641b4
JohnB19 <47255732+JohnB19@users.noreply.github.com>
2019-03-04 23:53:07 -0500

    UI fix

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let taskstructure = [
+{
+  time: `8:00 Am`,
+  thetask: `Pickup kids at school`,
+},
+{
+  time: `11:30 Am`,
+  thetask:`Meeting`,
+},
+{
+  time: `4:30 Pm`,
+  thetask: `Pick up Anna`,
+
+},
+];
+
+
+console.log( dailytask[1].id );
