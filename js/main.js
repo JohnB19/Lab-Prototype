62f127ac752916c342831c17f6b4a3c4286d2e1f
JohnB19 <47255732+JohnB19@users.noreply.github.com>
2019-03-14 23:30:34 -0400

    Data set
    Applying to application

--- js/main.js
+++ js/main.js
@@ -14,19 +14,41 @@ var taskstructure = [
   }
 ];
 
+// var taskstructureTuesday = [
+//   {
+
+//     time: `9:00 Am`,
+//     thetask: `Pickup kids at school`
+//   },
+//   {
+//     time: `11:30 Am`,
+//     thetask:`Meeting Tuesday`,
+//   },
+//   {
+//     time: `4:30 Pm`,
+//     thetask: `Pick up Anna`,
+//   }
+// ];
+ console.log(taskstructure);
+
+// console.log(taskstructureTuesday);
+
+
 var addTask = function() {
-    var allTask = [];
-    for (var i = taskstructure.length - 1; i >= 0; i--) {
+  var allTask = [];
+   for (var i = taskstructure.length - 1; i >= 0; i--) {
       var task = '<li class="tsk"><div class="time">'+taskstructure[i].time+'</div> <div class="task">'+ taskstructure[i].thetask+'</div></li>';
       allTask.push(task)
     }
 
     //document.getElementsByClassName('.Daytask1').innerHTML(allTask)
     $('.Daytask1').html(allTask);
+};
 
+// let htmlOutput = taskstructureTuesday.map(dateTuesday => `<p>${dateTuesday.time}  ${dateTuesday.thetask}</p>`).join(" ");
 
-
-}
+// document.getElementById('cTask1').innerHTML = htmlOutput;
+// console.log(allTask);
 
 addTask();
 //Js
