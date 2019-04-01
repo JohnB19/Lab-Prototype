7462c306541454207cf01d0c78f6d94b4d513277
JohnB19 <47255732+JohnB19@users.noreply.github.com>
2019-04-01 00:53:58 -0400

    Day tasks update
    Fix and updates to day tasks

--- js/main.js
+++ js/main.js
@@ -1,18 +1,71 @@
-let taskstructure = [
-{
-  time: `8:00 Am`,
-  thetask: `Pickup kids at school`,
-},
-{
-  time: `11:30 Am`,
-  thetask:`Meeting`,
-},
-{
-  time: `4:30 Pm`,
-  thetask: `Pick up Anna`,
+var taskstructure = [
+  {
 
-},
+    time: `8:00 Am`,
+    thetask: `Pickup kids at school`
+  },
+  {
+    time: `11:30 Am`,
+    thetask:`Meeting`,
+  },
+  {
+    time: `4:30 Pm`,
+    thetask: `Pick up Anna`,
+  }
 ];
 
+console.log(taskstructure);
 
-console.log( dailytask[1].id );
+var taskstructureTuesday = [
+  {
+
+    time: `4:00 Pm`,
+    thetask: `Gym Day`
+  },
+  {
+    time: `5:30 Pm`,
+    thetask:`Car Maintenance`,
+  },
+
+];
+console.log(taskstructureTuesday);
+
+
+var addTask = function() {
+  var allTask = [];
+   for (var i = taskstructure.length - 1; i >= 0; i--) {
+      var task = '<li class="tsk"><div class="time">'+taskstructure[i].time+'</div> <div class="task">'+ taskstructure[i].thetask+'</div></li>';
+      allTask.push(task)
+    }
+
+    //document.getElementsByClassName('.Daytask1').innerHTML(allTask)
+    $('.Daytask1').html(allTask);
+};
+
+// let htmlOutput = taskstructureTuesday.map(dateTuesday => `<p>${dateTuesday.time}  ${dateTuesday.thetask}</p>`).join(" ");
+
+// document.getElementById('cTask1').innerHTML = htmlOutput;
+// console.log(allTask);
+
+addTask();
+//Js
+// document.getElementById('alltasks');
+//jQuery
+// $('#alltasks');
+
+$('body').on('click', '.tsk', function() {
+   $(this).toggleClass("completed");
+});
+
+$('body').on('click', '.addTask', function() {
+    var taskTime = $('.inputTaskTime').val();
+    var taskName = $('.inputTaskName').val();
+
+    taskstructure.push({
+        time: taskTime,
+        thetask: taskName,
+    });
+
+    addTask();
+
+});
